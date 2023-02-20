1125b13ade327347e854feccbf7da45f040ea3d8
Maryen <voujum@gmail.com>
2023-02-20 16:46:54 +0100

    Front menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,21 @@
+import '../assets/logo.svg';
+import '../assets/icon-x-dark.svg';
+import '../assets/icon-o-dark.svg';
 import '../styles/styles.scss';
 
-console.log("Hello fffWorldds!");
-// console.log('hihihi');
+let xSelected = document.querySelector('.player-selection__x-selected');
+let oSelected = document.querySelector('.player-selection__o-selected');
+let slider = document.querySelector('.player-selection__choice__slider');
+
+function toggleSelectedPlayer() {
+  if (this == xSelected) {
+    slider?.classList.remove('o-selected');
+    slider?.classList.add('x-selected');
+  } else {
+    slider?.classList.add('o-selected');
+    slider?.classList.remove('x-selected');
+  }
+}
+
+oSelected?.addEventListener('click', toggleSelectedPlayer);
+xSelected?.addEventListener('click', toggleSelectedPlayer);
